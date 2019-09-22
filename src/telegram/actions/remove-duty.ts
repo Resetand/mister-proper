cc17f1f0fdd1efc94cfc01a6072c9bf60e28a348
resetand <machehin00@gmail.com>
2019-09-22 17:38:04 +0300

    deploy fix-4

--- src/telegram/actions/remove-duty.ts
+++ src/telegram/actions/remove-duty.ts
@@ -2,5 +2,5 @@ import { Message } from "node-telegram-bot-api";
 import { sendMessageWithLogs } from "../helper";
 
 export default async (message: Message) => {
-    await sendMessageWithLogs(message.chat.id, 'Серега');
+    await sendMessageWithLogs(message.chat.id, '...');
 };
