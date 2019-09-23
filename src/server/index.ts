5b4495ca3c5540a707f46fd7630d4b79f3e54188
resetand <machehin00@gmail.com>
2019-09-23 11:28:40 +0300

    fix server bootsrap

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,21 +3,18 @@ import http = require('http');
 import config from "../config";
 import { makeTBot } from '../telegram';
 import app from "./app";
-const server = http.createServer(app);
-const port = process.env.PORT || 4000;
-export const tbot = makeTBot();
 
 // https://www.npmjs.com/package/heroku-nosleep
 // For unable heroku inactive sleep
 
-noSleep(`${config.heroku.appUrl}/ping`);
+
+const server = http.createServer(app);
+const port = process.env.PORT || 4000;
+export const tbot = makeTBot();
+
 
 server.listen(port, async (err: Error) => {
-    if (err) {
-        console.error('Server startup failed');
-        console.error(err);
-        return;
-    }
+    noSleep(`${config.heroku.appUrl}/ping`);
 });
 
 
--- src/telegram/subscriptions.ts
+++ src/telegram/subscriptions.ts
@@ -1,16 +1,16 @@
 
-import onText from './controllers/text'
-import newChatMember from './controllers/new-chat-members'
-import leftChatMember from './controllers/left-chat-member'
-import groupChatCreated from './controllers/group-chat-created'
-import TelegramBot = require('node-telegram-bot-api')
+import TelegramBot = require('node-telegram-bot-api');
+import groupChatCreated from './controllers/group-chat-created';
+import leftChatMember from './controllers/left-chat-member';
+import newChatMember from './controllers/new-chat-members';
+import onText from './controllers/text';
 
 export const eventSubscribe = (tbot: TelegramBot) => {
-    tbot.on('group_chat_created', groupChatCreated)
-    tbot.on('new_chat_members', newChatMember)
-    tbot.on('left_chat_member', leftChatMember)
+    tbot.on('group_chat_created', groupChatCreated);
+    tbot.on('new_chat_members', newChatMember);
+    tbot.on('left_chat_member', leftChatMember);
     tbot.on('text', onText);
-}
+};
 
 
 
