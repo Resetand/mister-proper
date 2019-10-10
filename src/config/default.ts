91add3d553aa2daeb17aba32da9bdc909a0cfa30
resetand <machehin00@gmail.com>
2019-10-10 22:07:21 +0300

    shedu;e change

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,30 +1,29 @@
 const config = {
-    tbot: {
-        accessKey: process.env.TBOT_KEY || '',
-        url: 't.me/mister_proper_bot',
-        botname: 'mister_proper_bot',
-        nicknames: ['Пропер', 'бот', 'смешарик'],
-        userId: 730142607,
-    },
-    heroku: {
-        appUrl: 'https://polar-island-74903.herokuapp.com/',
-    },
-    // Пока нет bd
-    room: {
-        members: [
-            { username: 'Vergil190202', name: 'Матвей', isDuty: false },
-            { username: 'nostereal', name: 'Артем', isDuty: false },
-            { username: 'DotKillah', name: 'Илья', isDuty: false },
-            { username: 'resetand', name: 'Кирилл', isDuty: false },
-            { username: 'vugar', name: 'Вугар ', isDuty: true },
-        ],
-    },
+  tbot: {
+    accessKey: process.env.TBOT_KEY || "",
+    url: "t.me/mister_proper_bot",
+    botname: "mister_proper_bot",
+    nicknames: ["Пропер", "бот", "смешарик"],
+    userId: 730142607
+  },
+  heroku: {
+    appUrl: "https://polar-island-74903.herokuapp.com/"
+  },
+  // Пока нет bd
+  room: {
+    members: [
+      { username: "Vergil190202", name: "Матвей", isDuty: false },
+      { username: "nostereal", name: "Артем", isDuty: false },
+      { username: "DotKillah", name: "Илья", isDuty: true },
+      { username: "resetand", name: "Кирилл", isDuty: false },
+      { username: "vugar", name: "Вугар ", isDuty: false }
+    ]
+  }
 };
 
 type RecursivePartial<T> = { [P in keyof T]?: RecursivePartial<T[P]> };
 
 export type Config = typeof config;
 export type ConfigOverride = RecursivePartial<Config>;
-
 
 export default config;
